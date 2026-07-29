f844faf76bf347e79f7875432e41dd4c7d30497b
Vihaan Kembhavi <vihaankembhavi12@gmail.com>
2026-07-29 17:31:40 +0200

    Add comments for explanations in confusion-matrix.py

--- confusion-matrix.py
+++ confusion-matrix.py
@@ -16,27 +16,25 @@ Bottom-Right: True Positive (TP)
 '''
 
 accuracy = metrics.accuracy_score(actual, predicted)
-print("Accuracy:", accuracy) # Percentage that was correctly predicted
-# (TN + TP) / total
+print("Accuracy:", accuracy) 
+# Percentage that was correctly predicted: (TN + TP) / total
 
 precision = metrics.precision_score(actual, predicted)
-print("Precision:", precision) # Percentage of predicted positives that were predicted correctly
-# TP / (TP + FP)
+print("Precision:", precision) 
+# Percentage of predicted positives that were predicted correctly: TP / (TP + FP)
 
 recall = metrics.recall_score(actual, predicted)
 print("Recall:", recall) 
-# Recall / Sensitivity = Percentage of actual positives that were correctly predicted
-# TP / (TP + FN)
+# Recall / Sensitivity = Percentage of actual positives that were correctly predicted: TP / (TP + FN)
 
 specificity = metrics.recall_score(actual, predicted, pos_label = 0)
-print("Specificity:", specificity) # Percentage of actual negatives that were correctly predicted
-# TN / (TN + FP)
+print("Specificity:", specificity) 
+# Percentage of actual negatives that were correctly predicted: TN / (TN + FP)
 
 f_score = metrics.f1_score(actual, predicted)
 print("F-score:", f_score) 
-# Score of how well the predictions were made. Used for imbalanced data where precision and recall are important
-# Harmonic mean of precision and sensitivity
-# (2 * Precision * Sensitivity) / (Precision + Sensitivity)
+'''Score of how well the predictions were made. Used for imbalanced data where precision and recall are important.
+Harmonic mean of precision and recall: 2 * Precision * Sensitivity / (Precision + Sensitivity)'''
 
 print(actual)
 print(predicted)
